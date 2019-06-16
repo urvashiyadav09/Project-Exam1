9d2e1c4be4645fbd6e6d00bf7089900486fba499
urvashiyadav09 <45082858+urvashiyadav09@users.noreply.github.com>
2019-06-17 00:53:27 +0200

    regEx single quotes removed

--- validation.js
+++ validation.js
@@ -4,15 +4,15 @@
       var inputVal2 =  document.forms['queryform']['Customerlname'].value;
 
       console.log(inputVal1);
-      var regExName = '/^[a-zA-Z]+$/';
+      var regExName = /^[a-zA-Z]+$/;
       boolInput1 = regExName.test(inputVal1);
       boolInput2 = regExName.test(inputVal2);
 
       var inputVal3 =  document.forms['queryform']['Customeremail'].value;
-      var regExEmail = '/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2-4}$/'
+      var regExEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2-4}$/;
       var inputVal4 =  document.forms['queryform']['Customerphone'].value;
       // only for Norwegian number
-      var regExPhone = '/^\+\(\d{2}\)\d{8}$/';
+      var regExPhone = /^\+\(\d{2}\)\d{8}$/;
 
       boolInput3 = regExEmail.test(inputVal1);
       boolInput4 = regExPhone.test(inputVal2);
